82f66da55823a6ac2d2e97e3d863361d7d4b0603
Quintana <angelquintana@gmail.com>
2024-04-17 11:49:43 -0500

    terminada

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'firebase_options.dart'; // Importación de las opciones de Firebase. Esto puede contener la configuración de Firebase para la plataforma específica.
+
+import 'firebase_options.dart'; 
 
 void main() async {
   await Firebase.initializeApp(
